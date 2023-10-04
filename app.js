4cab15d47a26fcaa468aa27243b0f309e4ca962f
phanther1 <144461089+phanther1@users.noreply.github.com>
2023-10-04 10:14:16 -0400

    bmi functions

---
+++ app.js
@@ -0,0 +1,29 @@
+//get values
+const weight = Number(prompt("Your weight here in kilograms"));
+const height = Number(prompt("Your height here in centimeters"));
+
+//formula
+const formula = function(weight, height){
+    return weight / (height*height/10000);
+}
+
+const BMI = function(weight, height){
+    const calc = formula(weight, height);
+
+    if (calc < 18.5) {
+        return "You smol smol, let me give you a free all you can eat buffet ticket";
+    }
+    else if (calc >= 18.5 && calc <25) { // if calc value is from 18.5 and under 25, displays following message
+        return "Thats very healthy, you do you";
+    }
+    else if (calc >= 25 && calc <30) {
+        return "Maybe you should cut back on those bags of chips in the pantry";
+    }
+    else {
+        return "I think it's time for some cardio at the gym";
+    }
+}
+
+const checkBMI = formula(weight, height);
+alert(`Your BMI result: ${checkBMI.toFixed(2)}`); //displays result, .toFixed(2) round to 2 decimals
+alert(BMI(weight, height));
