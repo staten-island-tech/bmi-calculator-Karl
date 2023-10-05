5be600806cc69995e01e1bd49aa365954ab43e67
Karl F <144461089+phanther1@users.noreply.github.com>
2023-10-04 22:46:06 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -10,19 +10,17 @@ function BMI (checkBMI){
     if(checkBMI < 18.5){
         return ("You smol smol, let me give you a free all you can eat buffet ticket");
     }
-    else if(checkBMI >= 18.5 && calc <25){
+    else if(checkBMI >= 18.5 && checkBMI < 25){
         return ("Thats very healthy, you do you");
     }
-    else if(checkBMI >= 25 && calc <30){
-        return ("Maybe you should cut back on those bags of chips in the pantryz");
+    else if(checkBMI >= 25 && checkBMI < 30){
+        return ("Maybe you should cut back on those bags of chips in the pantrys");
     }
     else {
         return ("I think it's time for some cardio at the gym");
     }
 }
 
-BMI(checkBMI(height, weight))
-
 console.log(username);
-console.log("BMI= " + checkBMI(height, weight));
-console.log("You are " + BMI (checkBMI));
+console.log(checkBMI(height, weight));
+console.log(" " + BMI(checkBMI(height, weight)));
